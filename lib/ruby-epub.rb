4466f11a6059e7ec18160d344ec803e7c2b754cc
banux <banux@garm.(none)>
2011-04-17 11:50:20 +0200

    get cover from multiple methode

--- lib/ruby-epub.rb
+++ lib/ruby-epub.rb
@@ -1,5 +1,5 @@
 require 'zip/zip'
-require 'rexml/document'
+require 'nokogiri'
 
 class Epub
    attr_accessor :title, :language, :publisher, :date, :rights, :creator
@@ -12,41 +12,92 @@ class Epub
 
    def get_opf(zipfile)
       container_file = zipfile.get_input_stream("META-INF/container.xml")
-      container = REXML::Document.new container_file
-      opf_path = container.root.elements["rootfiles/rootfile"].attributes["full-path"]
+      container = Nokogiri::XML container_file
+      opf_path = container.at_css("rootfiles rootfile")['full-path']
+      tab_path = opf_path.split('/')
+      @base_path = ''
+      if tab_path.size > 1 
+         @base_path = tab_path[0] + '/'
+      end
       opf_file = zipfile.get_input_stream(opf_path)
-      @opf = REXML::Document.new opf_file  
+      @opf = Nokogiri::XML opf_file  
    end
 
    def get_metadata()
-    if @opf.root.elements["metadata/dc:title"]
-      @title = @opf.root .elements["metadata/dc:title"].text
+    if @opf.at_css("dc|title")
+      @title = @opf.at_css("dc|title").content
     end
-    if @opf.root.elements["metadata/dc:language"]
-      @language = @opf.root.elements["metadata/dc:language"].text
+    if @opf.at_css("dc|language")
+      @language = @opf.at_css("dc|language").content
     end
-    if @opf.root.elements["metadata/dc:publisher"]
-      @publisher = @opf.root.elements["metadata/dc:publisher"].text
+    if @opf.at_css("dc|publisher")
+      @publisher = @opf.at_css("dc|publisher").content
     end
-    if @opf.root.elements["metadata/dc:date"]
-      @date = @opf.root.elements["metadata/dc:date"].text
+    if @opf.at_css("dc|date")
+      @date = @opf.at_css("dc|date").content
     end
-    if @opf.root.elements["metadata/dc:rights"]
-      @rights = @opf.root.elements["metadata/dc:rights"].text
+    if @opf.at_css("dc|rights")
+      @rights = @opf.at_css("dc|rights").content
     end
-    if @opf.root.elements["metadata/dc:creator"]
-      @creator = @opf.root.elements["metadata/dc:creator"].text
+    if @opf.at_css("dc|creator")
+      @creator = @opf.at_css("dc|creator").content
     end
    end
  
    def cover_image
-    img_item = @opf.root.elements["manifest/item[@id='cover-image']"]
-    if img_item
-      img_url = 'OEBPS/' + img_item.attributes['href']
-puts img_url
-      @image_cover = @zip.get_input_stream(img_url) {|f| f.read}
-      return @image_cover
+     content = cover_by_cover_id
+     return content if content
+     content = cover_by_meta_cover
+     return content if content
+     content = cover_image_by_html
+     return content if content 
+   end
+
+   def cover_image_by_html
+    cover_item = @opf.at_css("package guide reference[@type='cover']")
+    if cover_item
+      cover_url = cover_item['href']
+      doc_cover = Nokogiri::HTML @zip.get_input_stream(@base_path + cover_url)
+      tab_path = cover_url.split('/')
+      html_path = ''
+      if tab_path.size > 1
+         html_path = tab_path[0] + '/'
+      end
+      img_src = doc_cover.xpath('//img').first
+      begin
+      	@image_cover = @zip.get_input_stream(@base_path + html_path + img_src['src']) {|f| f.read}
+        return @image_cover
+      rescue
+        return nil
+      end
     end
    end
+
+   def cover_by_meta_cover
+    img_id = @opf.at_css("meta[@name='cover']")
+    if img_id
+    img_item = @opf.at_css("manifest item[id='"+ img_id['content'] + "']")
+    if img_item
+      img_url = @base_path + img_item['href']
+      @image_cover = @zip.get_input_stream(img_url) {|f| f.read}
+      return @image_cover
+    else
+     return nil
+    end
+    end
+   end
+
+   def cover_by_cover_id
+    img_item = @opf.at_css("manifest item #cover")
+    if img_item
+      img_url = img_item['href']
+      puts img_url
+      @image_cover = @zip.get_input_stream(img_url) {|f| f.read}
+      return @image_cover
+    else
+     return nil
+    end
+   end
+
 
 end
--- test.rb
+++ test.rb
@@ -1,5 +1,16 @@
-load 'ruby-epub.rb'
+load 'lib/ruby-epub.rb'
+
+
+def info_epub(epub)
+puts epub.title + ' by ' + epub.creator
+cover = epub.cover_image
+if cover && cover.size > 0
+	puts "have cover"
+end
+end
 
 epub = Epub.new('test.epub')
-puts epub.title + ' by ' + epub.creator
+info_epub epub
+epub = Epub.new('test2.epub')
+info_epub epub
 
